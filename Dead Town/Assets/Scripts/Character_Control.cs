68565fbbc48a7732f8b1f3c4c87345bdad4aac7b
Gian-Dev2020 <72135815+Gian-Dev2020@users.noreply.github.com>
2021-04-03 23:20:06 +0100

    Movement v1
    Added movement, still in progress.

---
+++ Dead Town/Assets/Scripts/Character_Control.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Control : MonoBehaviour
+{
+    public CharacterController controller; // references the controller from unity
+
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When run it'll range from -1 to 1 (-1 being either the left of down axis)
+        float Horizontal = Input.GetAxisRaw("Horizontal"); //Raw axis does not give any input smoothing
+        float Vertical = Input.GetAxisRaw("Vertical");
+
+        // Vector 3 works on x, y, z. So Horizontal would be x, 0 would be y, and Vertical would be z
+        // normalized ensures that if the character moves diagonaly it retains the same speed
+        Vector3 direction = new Vector3(Horizontal, 0f, Vertical).normalized; 
+
+        if (direction.magnitude >= 0.1f)
+        {
+            // Will move the character.
+            // Time.deltaTime is from the last frame, it makes independent to the amount of time and frames since the character moved.
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ Dead Town/Assets/Scripts/SCRIPT.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCRIPT : MonoBehaviour
+{
+    [SerializeField] // allows the variable to edited and seen in the Unity inspector
+    int lives;
+    [SerializeField]
+    float Mana;
+    [SerializeField]
+    bool Live;
+    [SerializeField]
+    string saysomething;
+
+    Rigidbody CubeRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CubeRB = GetComponent<Rigidbody>();
+
+
+        // if the game is run these values will overwrite any values set in the inspector in Unity
+        lives = 10;
+        Mana = 100;
+        Live = true;
+        saysomething = "Yo";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    void movement()
+    {
+        float x;
+        x = 0;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            x += 1;
+            Debug.Log("Key is pressed" + x);
+        }
+    }
+}
