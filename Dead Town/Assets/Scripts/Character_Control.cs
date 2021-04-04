457ddede6376e94f8c51a71c30813cee6f197c46
Gian-Dev2020 <72135815+Gian-Dev2020@users.noreply.github.com>
2021-04-04 20:23:40 +0100

    Movement v2
    Added animation and rotation.

--- Dead Town/Assets/Scripts/Character_Control.cs
+++ Dead Town/Assets/Scripts/Character_Control.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Character_Control : MonoBehaviour
 {
     public CharacterController controller; // references the controller from unity
-
+    
     public float speed = 5f;
+    
 
     // Update is called once per frame
     void Update()
+    {
+        movement();
+    }
+
+    void movement()
     {
         // When run it'll range from -1 to 1 (-1 being either the left of down axis)
         float Horizontal = Input.GetAxisRaw("Horizontal"); //Raw axis does not give any input smoothing
@@ -17,15 +23,16 @@ public class Character_Control : MonoBehaviour
 
         // Vector 3 works on x, y, z. So Horizontal would be x, 0 would be y, and Vertical would be z
         // normalized ensures that if the character moves diagonaly it retains the same speed
-        Vector3 direction = new Vector3(Horizontal, 0f, Vertical).normalized; 
+        Vector3 direction = new Vector3(Horizontal, 0f, Vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
             // Will move the character.
             // Time.deltaTime is from the last frame, it makes independent to the amount of time and frames since the character moved.
             controller.Move(direction * speed * Time.deltaTime);
+
+            float Angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
-
-
     }
 }
