7fda0ace67942838d36e450ae0a8f10567d9a0da
Gian-Dev2020 <72135815+Gian-Dev2020@users.noreply.github.com>
2021-04-07 20:30:23 +0100

    Maya - Mountains v2
    Added another maya modelled mountain

--- Dead Town/Assets/Scripts/Character_Control.cs
+++ Dead Town/Assets/Scripts/Character_Control.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Character_Control : MonoBehaviour
 {
     public CharacterController controller; // references the controller from unity
-    
+    public Animator animate;
+
     public float speed = 5f;
     
 
@@ -20,6 +21,8 @@ public class Character_Control : MonoBehaviour
         // When run it'll range from -1 to 1 (-1 being either the left of down axis)
         float Horizontal = Input.GetAxisRaw("Horizontal"); //Raw axis does not give any input smoothing
         float Vertical = Input.GetAxisRaw("Vertical");
+        animate = this.GetComponent<Animator>();
+        animate.speed = 0;
 
         // Vector 3 works on x, y, z. So Horizontal would be x, 0 would be y, and Vertical would be z
         // normalized ensures that if the character moves diagonaly it retains the same speed
@@ -30,9 +33,13 @@ public class Character_Control : MonoBehaviour
             // Will move the character.
             // Time.deltaTime is from the last frame, it makes independent to the amount of time and frames since the character moved.
             controller.Move(direction * speed * Time.deltaTime);
+            animate.SetBool("Move", true);
+            animate.speed = 1;
+
 
             float Angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, Angle, 0f);
         }
+
     }
 }
