d1663572670d21b169fafb0fc6dcc179c8423847
Gian-Dev2020 <72135815+Gian-Dev2020@users.noreply.github.com>
2021-04-08 15:59:17 +0100

    Animation v10
    Updated some animations and tested other ways to apply Mixamo animations

--- Dead Town/Assets/Scripts/Character_Control.cs
+++ Dead Town/Assets/Scripts/Character_Control.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Character_Control : MonoBehaviour
 {
     public CharacterController controller; // references the controller from unity
-    public Animator animate;
 
     public float speed = 5f;
     
@@ -21,8 +20,6 @@ public class Character_Control : MonoBehaviour
         // When run it'll range from -1 to 1 (-1 being either the left of down axis)
         float Horizontal = Input.GetAxisRaw("Horizontal"); //Raw axis does not give any input smoothing
         float Vertical = Input.GetAxisRaw("Vertical");
-        animate = this.GetComponent<Animator>();
-        animate.speed = 0;
 
         // Vector 3 works on x, y, z. So Horizontal would be x, 0 would be y, and Vertical would be z
         // normalized ensures that if the character moves diagonaly it retains the same speed
@@ -32,13 +29,12 @@ public class Character_Control : MonoBehaviour
         {
             // Will move the character.
             // Time.deltaTime is from the last frame, it makes independent to the amount of time and frames since the character moved.
-            controller.Move(direction * speed * Time.deltaTime);
-            animate.SetBool("Move", true);
-            animate.speed = 1;
+            controller.Move(speed * direction * Time.deltaTime);
 
+            // Code below will make the character face the direction of the movement
+            float target = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, target, 0f);
 
-            float Angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, Angle, 0f);
         }
 
     }
