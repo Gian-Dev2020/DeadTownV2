ca19ae606b541ea1bffba11ecab8844901436612
Gian-Dev2020 <72135815+Gian-Dev2020@users.noreply.github.com>
2021-05-04 11:07:07 +0100

    Tool_Made v1
    Extra tool made(just simple one)

---
+++ Dead Town/Assets/Scripts/My_Tool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class My_Tool : EditorWindow
+{
+    string ObjectName = "";
+    int ObjectID = 1;
+    GameObject ObjectToSpawn;
+    float Scale;
+    float Radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
